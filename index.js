167cf48f0eb888538a4335ce354ffa0e55526b05
zitongliu <zitongliu7@gmail.com>
2017-10-27 14:17:34 +1100

    initial server setup

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+// instance of express
+const app = express();
+
+// App Setup - getting express working
+
+
+// Server Setup - getting express to talk to outside world
+const port = process.env.PORT || 3090;
+// http is a native node library
+// create a http server that knows how to receive a request and forward it to the express application
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
