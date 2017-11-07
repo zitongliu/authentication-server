5893954ab75adad1a01adde0aaa674710095255b
zitongliu <zitongliu7@gmail.com>
2017-11-07 14:18:15 +1100

    set up bcrypt password encryption

--- models/user.js
+++ models/user.js
@@ -1,10 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define our model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// On Save Hook, encrypt password
+// Before saving a model, run this function
+userSchema.pre('save', function(next) {
+  // get access to the user model
+  const user = this; // user.email, user.password
+
+  // generate a salt
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    // hash (encrypt) our password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash){
+      if (err) { return next(err); }
+
+      // overwrite plain tet password with encrypted password
+      user.password = hash;
+      // go ahead and save the model
+      next();
+    });
+  });
 });
 
 // Create the model class
